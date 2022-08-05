5a166007af5a88894f1c438103d831945c192fb1
afeiship <1290657123@qq.com>
2022-08-05 10:40:12 +0800

    feat: add antd-i18n

---
+++ generators/antd-i18n/index.js
@@ -0,0 +1,16 @@
+'use strict';
+const Generator = require('@jswork/yeoman-generator');
+
+module.exports = class extends Generator {
+  writing() {
+    this.fs.copyTpl(this.srcFiles, this.destinationPath(), this.props);
+  }
+
+  end() {
+    this.extendJSON({
+      dependencies: {
+        '@jswork/react-ant-i18n': 'latest'
+      }
+    });
+  }
+};
